00c6b0fc37c4f6c06a5e84db60d7c14da891e1b7
Hrithik Varshney <hritik.varshney2000@gmail.com>
2023-08-27 19:10:29 +0530

    navbar added

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar_comp">
+        <div className="navbar_name">
+          <span className="firstName">
+               Hrithik
+          </span>
+          <span className="lastName">
+            Varshney
+          </span>
+        </div>
+        <div className="navbar_route">
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../component/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home_page'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
