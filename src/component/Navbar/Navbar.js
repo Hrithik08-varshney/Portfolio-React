06a8e852db434a514ed2d7ab75153d66a73f2107
Hrithik Varshney <hritik.varshney2000@gmail.com>
2023-09-02 19:03:32 +0530

    added css for navigation bar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,18 +1,43 @@
 import React from 'react'
-
+import "./style.css";
 const Navbar = () => {
+  const routeArr=[
+    {
+      name:"Home"
+    },
+    {
+      name:"About"
+    },
+    {
+      name:"Projects"
+    },
+    {
+      name:"Skills"
+    },
+    {
+      name:"Contact"
+    }
+  ]
   return (
-    <div className="navbar_comp">
-        <div className="navbar_name">
-          <span className="firstName">
+    <div className="navbar_comp flexCommon">
+        <div className="navbar_name df jcsb">
+          <div className="firstName">
                Hrithik
-          </span>
-          <span className="lastName">
+          </div>
+          <div className="lastName">
             Varshney
-          </span>
+          </div>
         </div>
-        <div className="navbar_route">
-
+        <div className="navbar_route df jcsa cpoint">
+           {
+            routeArr.map((item,index)=>{
+                return (
+                  <span className="routeValues" key={item+index}>
+                    {item.name}
+                  </span>
+                )
+            })
+           }
         </div>
     </div>
   )
