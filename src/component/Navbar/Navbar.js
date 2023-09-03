82e714d4addc64240a1fdbe529aad9d5bc2fdf31
Hrithik Varshney <hritik.varshney2000@gmail.com>
2023-09-03 14:08:55 +0530

    added media query for nav

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,46 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import "./style.css";
+import { FaGripHorizontal } from "react-icons/fa";
+import myImage from "../../assets/images/myImage.jpeg";
 const Navbar = () => {
-  const routeArr=[
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const routeArr = [
     {
-      name:"Home"
+      name: "Home",
     },
     {
-      name:"About"
+      name: "Experience",
     },
     {
-      name:"Projects"
+      name: "Projects",
     },
     {
-      name:"Skills"
+      name: "Skills",
     },
     {
-      name:"Contact"
-    }
-  ]
+      name: "Contact",
+    },
+  ];
   return (
     <div className="navbar_comp flexCommon">
-        <div className="navbar_name df jcsb cpoint">
-          <div className="firstName">
-               Hrithik
+      {windowWidth > 1200 ? (
+        <>
+          <div className="navbar_name df jcsb cpoint">
+            <div className="firstName">Hrithik</div>
+            <div className="lastName">Varshney</div>
           </div>
-          <div className="lastName">
-            Varshney
+          <div className="navbar_route df jcsa cpoint">
+            {routeArr.map((item, index) => {
+              return (
+                <span className="routeValues" key={item + index}>
+                  {item.name}
+                </span>
+              );
+            })}
           </div>
-        </div>
-        <div className="navbar_route df jcsa cpoint">
-           {
-            routeArr.map((item,index)=>{
-                return (
-                  <span className="routeValues" key={item+index}>
-                    {item.name}
-                  </span>
-                )
-            })
-           }
-        </div>
+        </>
+      ) : (
+        <>
+          <div className="diff_Nav_View df jcsb">
+            <div className="img_div">
+              <img src={myImage} alt="myImage" className="myImage" />
+            </div>
+            <div className="navbar_name df jcsb cpoint">
+              <div className="firstName">Hrithik</div>
+              <div className="lastName">Varshney</div>
+            </div>
+            <div className="icon_div">
+              <FaGripHorizontal />
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
