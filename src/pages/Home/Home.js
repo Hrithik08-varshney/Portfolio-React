84dd18eec1f5366d7911deea48f146605ad1fadb
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-05-07 00:01:46 +0530

    latest portfolio changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../component/Navbar/Navbar'
+import About from '../../component/About/About'
 
 const Home = () => {
   return (
     <div className='home_page'>
       <Navbar/>
+      <About/>
     </div>
   )
 }
